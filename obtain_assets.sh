69cde91673a0067a417f9a6d1886139b30485664
mospy26 <m.mustafaahmed@hotmail.com>
2022-08-22 16:16:23 +1000

    Modified obtain_assets.sh to only obtain assets when needed

--- obtain_assets.sh
+++ obtain_assets.sh
@@ -1,4 +1,14 @@
-echo "Creating assets folder..."
-mkdir assets
-echo "Obtaining git-prompt.sh script..."
-cd assets/ && wget https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
+
+if [ ! -d assets ]; then
+    echo "Creating assets folder..."
+    mkdir assets
+else
+    echo "assets/ folder already exists, skipping creation..."
+fi
+
+if [ ! -f assets/git-prompt.sh ]; then
+    echo "Obtaining git-prompt.sh script..."
+    cd assets/ && wget https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
+else
+    echo "git-prompt.sh script exists, skipping download..."
+fi

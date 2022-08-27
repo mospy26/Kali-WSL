9df63f2a6de00e1b47c5c6562ce6a8f2ae3cfe94
mospy26 <m.mustafaahmed@hotmail.com>
2022-08-28 03:04:25 +1000

    Added a new scripts folder that contains a work.sh (for work) and customise.sh (for non work PC) scripts that are to be sourced in bashrc files.
    Modified run script to load all of this. run.sh is optimised to use a
    decorator now.

--- run.sh
+++ run.sh
@@ -1,14 +1,60 @@
-echo "Obtaining necessary assets..."
-bash obtain_assets.sh
-echo "Done."
+# Run this script to setup your WSL environment
 
-echo "Copying assets into relevant locations..."
-cp assets/git-prompt.sh ~/.git-prompt.sh
-echo "Done."
+# Decorator
+decorate() {
+	eval "
+		_inner_$(typeset -f $1)
+		$1"'() {
+			echo >&2 "$1"
+			_inner_'"$1"' "$@"
+			local ret=$?
+			if [ $ret -ne 0 ]; then
+				echo "An error occurred!"
+			else
+				echo >&2 "Done."
+			fi
+			return "$ret"
+		}'
+	}
 
-echo "Copying bashrc and tmux config files..."
-cp .bashrc ~/.bashrc
-cp .tmux.conf ~/.tmux.conf
-echo "Done."
+obtain_assets() {
+	bash obtain_assets.sh
+	return $?
+}
+decorate obtain_assets
+obtain_assets "Obtaining necessary assets..." 
 
-echo "Setup complete."
+copyAssets() {
+	mkdir -p ~/assets && cp ./assets/git-prompt.sh ~/assets
+}
+decorate copyAssets
+copyAssets "Copying assets into relevant locations..."
+
+# Copies bashrc and tmux.conf files into home dir
+copyCoreConfigFiles() {
+	cp .bashrc ~/.bashrc
+	cp .tmux.conf ~/.tmux.conf
+}
+decorate copyCoreConfigFiles
+copyCoreConfigFiles "Copying bashrc and tmux config files..." 
+
+read -p "Is this your work environment (default: no, choose yes/no)? " isWorkEnv
+isWorkEnv=${isWorkEnv:-no} # default value is no
+
+copyScripts() {
+	printf "\n" >> ~/.bashrc
+	echo "#########################################################################" >> ~/.bashrc
+	echo "######################### Source custom scripts #########################" >> ~/.bashrc
+	echo "#########################################################################" >> ~/.bashrc
+	printf "\n" >> ~/.bashrc
+	mkdir -p ~/scripts && cp ./scripts/customised.sh ~/scripts/customised.sh
+	echo "source ~/scripts/customised.sh" >> ~/.bashrc	
+	if [ "$isWorkEnv" == "yes" ]; then
+		mkdir -p ~/scripts && cp ./scripts/work.sh ~/scripts
+		echo "source ~/scripts/work.sh" >> ~/.bashrc
+	fi
+}
+decorate copyScripts
+copyScripts "Copying necesary scripts and modifying bashrc..."
+
+echo "Setup complete. Enjoy!"
---
+++ scripts/work.sh
@@ -0,0 +1,25 @@
+# This is a script relevant to specific machines at work only
+
+####################################################################################
+######################## ALL CUSTOM CHANGES BEGIN HERE #############################
+####################################################################################
+
+export http_proxy=http://wcmproxy.pgo.local:80/
+export https_proxy=http://wcmproxy.pgo.local:80/
+
+
+################################# variables ########################################
+ 
+D_PROJECTS="/mnt/d/Projects"
+C_REPOS="/mnt/c/repos"
+C_HOME="/mnt/c/Users/mmustafa"
+
+
+# Project local directories
+alias cdec="cd $D_PROJECTS/e4.6"
+alias cdhatch3="cd $C_REPOS/Hatch3"
+alias cdbernina="cd $C_REPOS/Bernina9"
+
+
+# Shortcut to scripts directory in local PC
+alias cdscripts="cd $C_HOME/My\ Documents/Scripts"

2be70ddd5873d55e1b527d1fbd069eb254f7b7cf
mospy26 <m.mustafaahmed@hotmail.com>
2022-08-30 22:54:36 +1000

    Setup tmux to open with specific layout using tmux attach + Alias grep to grep with color

--- scripts/customised.sh
+++ scripts/customised.sh
@@ -5,6 +5,7 @@
 # Shortcuts to windows C and D drive from linux
 alias cdc="cd /mnt/c"
 alias cdd="cd /mnt/d"
+alias grep="grep --color='auto'"
 
 # grep files from list of tracked files on git
 alias gitgrep="git ls-files | grep $@"
